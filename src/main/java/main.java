3349b43997ac4ec90f8c686e5ad4280ece349d48
User <User@192.168.0.206>
2020-11-11 15:30:03 +0000

    Changed Hello World message

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -11,7 +11,7 @@
 public class main {
     
     public static void main (String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello IS3313");
         
     }
 }
